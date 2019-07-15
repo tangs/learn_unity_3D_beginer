d8bacbffff29321e51e5c6cc92dfb299c3335953
tangs <tangsong1988kaka@gmail.com>
2019-07-15 12:28:52 +0800

    update.

--- Assets/Scripts/JohnLemon.cs
+++ Assets/Scripts/JohnLemon.cs
@@ -4,27 +4,51 @@ using UnityEngine;
 
 public class JohnLemon : MonoBehaviour
 {
-    private Animator animator;
-    private Transform transform;
+    private Animator m_Animator;
+    private Rigidbody m_Rigidbody;
+    //private Transform transform;
+    private Vector3 m_Movement;
+    private Quaternion m_Rotation = Quaternion.identity;
+
+    public float turnSpeed = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        transform = GetComponent<Transform>();
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        //transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        bool isWalk = Input.GetAxis("Vertical") > 0;
-        transform.Rotate(new Vector3(0, horizontal, 0));
-        animator.SetBool("IsWalking", isWalk);
-        //Debug.Log("update.");
-    }
-
     void FixedUpdate()
     {
-        //Debug.Log("fixed update.");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(0f, horizontal);
+        bool hasVerticalInput = !Mathf.Approximately(0f, vertical);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        //bool isWalk = vertical > 0;
+        //transform.Rotate(new Vector3(0, horizontal, 0));
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward,
+            m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+        //Debug.Log(m_Movement);
+        Debug.Log(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position
+            + m_Movement * m_Animator.deltaPosition.magnitude);
+        //Debug.Log(m_Rotation);
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
